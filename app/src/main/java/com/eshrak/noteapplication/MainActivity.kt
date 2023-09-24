b8de3c4eaee09e55d22334027717d50a9f7bd47f
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-09-24 23:10:13 +0600

    Project Build

---
+++ app/src/main/java/com/eshrak/noteapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eshrak.noteapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

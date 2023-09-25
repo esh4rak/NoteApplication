188cdca626fcba45197f4c1144c71e4a28f90747
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-09-25 14:25:40 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/ui/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/eshrak/noteapplication/ui/viewmodels/AuthViewModel.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
 @HiltViewModel
 class AuthViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
 

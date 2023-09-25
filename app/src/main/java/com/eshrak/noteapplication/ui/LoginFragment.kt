6a8b9ea84e5d92d7113254841c6d316531a073a3
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-09-25 11:46:25 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/ui/LoginFragment.kt
+++ app/src/main/java/com/eshrak/noteapplication/ui/LoginFragment.kt
@@ -5,17 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.eshrak.noteapplication.R
+import com.eshrak.noteapplication.databinding.FragmentLoginBinding
+
 
 class LoginFragment : Fragment() {
+
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
--- app/src/main/java/com/eshrak/noteapplication/ui/RegisterFragment.kt
+++ app/src/main/java/com/eshrak/noteapplication/ui/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.eshrak.noteapplication.R
 import com.eshrak.noteapplication.databinding.FragmentRegisterBinding
 
 
@@ -14,13 +16,15 @@ class RegisterFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
 
 
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
 
         return binding.root
 

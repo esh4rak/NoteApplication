c5dc20ccf078755ea8bdd62041e039c754d87f4f
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-09-25 15:01:27 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/util/Constants.kt
+++ app/src/main/java/com/eshrak/noteapplication/util/Constants.kt
@@ -3,6 +3,6 @@ package com.eshrak.noteapplication.util
 object Constants {
 
     const val TAG = "KOTLIN_CODE"
-    const val BASE_URL = "http://localhost:5000"
+    const val BASE_URL = "http://10.0.2.2:5000/"
 
 }

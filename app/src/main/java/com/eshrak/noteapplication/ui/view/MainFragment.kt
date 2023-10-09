b2042304501c69c3782ad7f7b05ce093f33d33b3
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-10-09 15:36:10 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/ui/view/MainFragment.kt
+++ app/src/main/java/com/eshrak/noteapplication/ui/view/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.eshrak.noteapplication.ui.view
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -54,13 +56,15 @@ class MainFragment : Fragment() {
         //Observers
         bindObservers()
 
-
         //Get Notes
         noteViewModel.getNotes()
 
-
         binding.addNote.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_noteFragment)
+        }
+
+        binding.menuButton.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
         }
 
     }

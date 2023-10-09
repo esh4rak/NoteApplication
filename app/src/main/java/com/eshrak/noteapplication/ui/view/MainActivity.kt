f0888b48bfb74f1131cb93791b32ee8ef0992d5c
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-10-09 12:55:17 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/ui/view/MainActivity.kt
+++ app/src/main/java/com/eshrak/noteapplication/ui/view/MainActivity.kt
@@ -2,39 +2,18 @@ package com.eshrak.noteapplication.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
 import com.eshrak.noteapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        //Force Test Crash
-        //testCrash()
     }
 
-    /*private fun testCrash() {
-        val crashButton = Button(this)
-        crashButton.text = "Test Crash"
-        crashButton.setOnClickListener {
-            throw RuntimeException("Test Crash") // Force a crash
-        }
-
-        addContentView(
-            crashButton, ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-        )
-    }*/
 }

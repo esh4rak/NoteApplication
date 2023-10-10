72500f29e6dfb0b9973f3f8a06ef6d3969599263
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-10-10 16:06:53 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/ui/view/MainFragment.kt
+++ app/src/main/java/com/eshrak/noteapplication/ui/view/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.eshrak.noteapplication.ui.view
 
-
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
@@ -16,7 +16,6 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.eshrak.noteapplication.R
 import com.eshrak.noteapplication.data.models.NoteResponse
 import com.eshrak.noteapplication.databinding.FragmentMainBinding
-import com.eshrak.noteapplication.databinding.NavHeaderBinding
 import com.eshrak.noteapplication.ui.adapters.NoteAdapter
 import com.eshrak.noteapplication.ui.viewmodels.NoteViewModel
 import com.eshrak.noteapplication.util.Constants.TAG
@@ -33,14 +32,8 @@ class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var navHeaderBinding: NavHeaderBinding
-
     private val noteViewModel by viewModels<NoteViewModel>()
-
-
     private lateinit var adapter: NoteAdapter
-
 
     @Inject
     lateinit var tokenManager: TokenManager
@@ -50,11 +43,8 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-
         _binding = FragmentMainBinding.inflate(inflater, container, false)
 
-        val navView = inflater.inflate(R.layout.nav_header, container, false)
-        navHeaderBinding = NavHeaderBinding.bind(navView)
         setNavigationDrawer()
 
         adapter = NoteAdapter(::onNoteClicked)
@@ -74,8 +64,15 @@ class MainFragment : Fragment() {
         //Observers
         bindObservers()
 
+        //Handler
+        bindHandler()
+
         //Get Notes
         noteViewModel.getNotes()
+
+    }
+
+    private fun bindHandler() {
 
         binding.addNote.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_noteFragment)
@@ -83,6 +80,34 @@ class MainFragment : Fragment() {
 
         binding.menuButton.setOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+
+        // Set up navigation item clicks
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    //ToDo
+                }
+
+                R.id.nav_settings -> {
+                    //ToDo
+                }
+
+                R.id.nav_share -> {
+                    //ToDo
+                }
+
+                R.id.nav_about -> {
+                    //ToDo
+                }
+
+                R.id.nav_logout -> {
+                    binding.drawerLayout.closeDrawers()
+                }
+            }
+
+            true
         }
 
     }
@@ -113,14 +138,18 @@ class MainFragment : Fragment() {
 
     private fun setNavigationDrawer() {
 
+        val headerView = binding.navigationView.getHeaderView(0)
+        val title = headerView.findViewById<TextView>(R.id.nav_drawer_title)
+        val subtitle = headerView.findViewById<TextView>(R.id.nav_drawer_subtitle)
+
         if (tokenManager.getUserName() != null) {
-            navHeaderBinding.navDrawerTitle.text = tokenManager.getUserName()
+            title.text = tokenManager.getUserName()
         } else {
             Log.d(TAG, "User Name Not Found")
         }
 
         if (tokenManager.getUserEmail() != null) {
-            navHeaderBinding.navDrawerSubtitle.text = tokenManager.getUserEmail()
+            subtitle.text = tokenManager.getUserEmail()
         } else {
             Log.d(TAG, "Email Not Found")
         }

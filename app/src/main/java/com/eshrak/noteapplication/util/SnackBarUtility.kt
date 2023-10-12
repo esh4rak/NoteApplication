28ca61186f1506fb5af415c1654969de3561a254
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-10-12 13:40:44 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/util/SnackBarUtility.kt
+++ app/src/main/java/com/eshrak/noteapplication/util/SnackBarUtility.kt
@@ -1,6 +1,5 @@
 package com.eshrak.noteapplication.util
 
-import android.graphics.Color
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
@@ -8,7 +7,9 @@ object SnackBarUtility {
     fun showSnackBar(view: View, message: String) {
 
         val snackBar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-        snackBar.view.setBackgroundColor(Color.parseColor("#8692f7"))
+        snackBar.setAction("OK") {
+            //ToDo
+        }
         snackBar.show()
     }
 }

5170bcb9920a7d86254b9f7c2d3b923609c36560
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-10-12 17:09:30 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/QuoteFragment.kt
+++ app/src/main/java/com/eshrak/noteapplication/QuoteFragment.kt
@@ -5,18 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.eshrak.noteapplication.databinding.FragmentQuoteBinding
 
 
 class QuoteFragment : Fragment() {
 
+    private var _binding: FragmentQuoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_quote, container, false)
+        _binding = FragmentQuoteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        //Observers
+        //bindObservers()
+
+        //Handler
+        bindHandler()
+
+        //Get Quotes
+        //noteViewModel.getNotes()
+    }
+
+
+    private fun bindHandler() {
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }

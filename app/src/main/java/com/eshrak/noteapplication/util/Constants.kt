2f8fb77e41242de635ce586709b0594c6323eaea
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-10-13 13:29:40 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/util/Constants.kt
+++ app/src/main/java/com/eshrak/noteapplication/util/Constants.kt
@@ -3,7 +3,7 @@ package com.eshrak.noteapplication.util
 object Constants {
 
     const val TAG = "KOTLIN_CODE"
-    const val BASE_URL = "http://10.0.2.2:5000/"
+    const val BASE_URL = "https://eshraknoteapi.onrender.com/"
     const val BASE_URL_2 = "https://api.quotable.io/"
     const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
     const val USER_TOKEN = "USER_TOKEN"

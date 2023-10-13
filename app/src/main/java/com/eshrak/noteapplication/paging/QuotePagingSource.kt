f8e03c6c0eb71752c51b384fcfd15d1417d67477
Sakib Eshrak <eshrak.sea4@gmail.com>
2023-10-13 16:59:16 +0600

    updated

--- app/src/main/java/com/eshrak/noteapplication/paging/QuotePagingSource.kt
+++ app/src/main/java/com/eshrak/noteapplication/paging/QuotePagingSource.kt
@@ -5,7 +5,11 @@ import androidx.paging.PagingState
 import com.eshrak.noteapplication.data.api.QuoteAPI
 import com.eshrak.noteapplication.data.models.Result
 
+
+const val STARTING_PAGE_INDEX = 1
+
 class QuotePagingSource(private val quoteAPI: QuoteAPI) : PagingSource<Int, Result>() {
+
     override fun getRefreshKey(state: PagingState<Int, Result>): Int? {
         return state.anchorPosition?.let {
             state.closestPageToPosition(it)?.prevKey?.plus(1)
@@ -17,12 +21,12 @@ class QuotePagingSource(private val quoteAPI: QuoteAPI) : PagingSource<Int, Resu
 
         return try {
 
-            val position = params.key ?: 1
+            val position = params.key ?: STARTING_PAGE_INDEX
             val response = quoteAPI.getQuotes(position)
 
             LoadResult.Page(
                 data = response.body()!!.results,
-                prevKey = if (position == 1) null else position - 1,
+                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
                 nextKey = if (position == response.body()!!.totalPages) null else position + 1
             )
 
